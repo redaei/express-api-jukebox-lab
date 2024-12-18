a1c913b201c3a5824ad8579ece7c4c1129b7433f
redaei <redaeis@gmail.com>
2024-12-18 20:39:25 +0300

    final

---
+++ controllers/tracks.js
@@ -0,0 +1,79 @@
+const Track = require('../models/Track.js')
+const express = require('express')
+const router = express.Router()
+
+router.post('/', async (req, res) => {
+  try {
+    const createdTrack = await Track.create(req.body)
+    res.status(201).json(createdTrack)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+router.get('/', async (req, res) => {
+  try {
+    const foundTracks = await Track.find()
+    res.status(200).json(foundTracks)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const foundTrack = await Track.findById(req.params.id)
+    if (!foundTrack) {
+      res.status(404)
+      throw new Error('Track not found.')
+    }
+    res.status(200).json(foundTrack)
+  } catch (error) {
+    if (res.statusCode === 404) {
+      res.json({ error: error.message })
+    } else {
+      res.status(500).json({ error: error.message })
+    }
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    let deletedTrack = await Track.findByIdAndDelete(req.params.id)
+
+    if (!deletedTrack) {
+      res.status(404)
+      throw new Error('Track not found!')
+    }
+    res.status(200).json({
+      message: `Successfully deleted track with title: ${deletedTrack.title}`
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedTrack = await Track.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true
+      }
+    )
+    if (!updatedTrack) {
+      res.status(404)
+      throw new Error('Track not found.')
+    }
+    res.status(200).json(updatedTrack)
+  } catch (error) {
+    if (res.statusCode === 404) {
+      res.json({ error: error.message })
+    } else {
+      res.status(500).json({ error: error.message })
+    }
+  }
+})
+
+module.exports = router
